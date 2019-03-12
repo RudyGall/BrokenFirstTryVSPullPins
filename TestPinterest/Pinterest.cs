eb745a332386427ea00a5e3edde5208ad4952110
Rudy Gall <rudyegall@gmail.com>
2019-03-12 10:30:55 -0400

    Totally broken code but is what we have for pinterest rn

---
+++ TestPinterest/Pinterest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace TestPinterest
+{
+    class Pinterest
+    {
+        public string AppIDSecret { get; set; }
+        public string AppID { get; set; }
+        public string AccessToken { get; set; }
+
+        public async Task<IEnumerable<string>> GetPins(string url, string first_name, string last_name)
+        {
+            if (AccessToken == null)
+            {
+                AccessToken = await GetAccessToken();
+            }
+
+            var requestUserTimeline = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.pinterest.com/v1/users/GcSocialmediatest/"));
+            requestUserTimeline.Headers.Add("Authorization", "Bearer " + AccessToken);
+            var httpClient = new HttpClient();
+            HttpResponseMessage responseUserTimeLine = await httpClient.SendAsync(requestUserTimeline);
+            var serializer = new JavaScriptSerializer();
+            dynamic json = serializer.Deserialize<object>(await responseUserTimeLine.Content.ReadAsStringAsync());
+            var enumerablePins = (json as IEnumerable<dynamic>);
+
+            if (enumerablePins == null)
+            {
+                return null;
+            }
+            return enumerablePins.Select(pin => (string)(pin["data"].ToString()));
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            var httpClient = new HttpClient();
+            //var request = new HttpRequestMessage(HttpMethod.Post, "https://api.pinterest.com/oauth2/token "); 
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://www.getpostman.com/oauth2/callback");
+            var customerInfo = Convert.ToBase64String(new UTF8Encoding().GetBytes(AppID + ":" + AppIDSecret));
+            request.Headers.Add("Authorization", "Basic " + customerInfo);
+            request.Content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            //HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
+
+            string json = await response.Content.ReadAsStringAsync();
+            var serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(json);
+            return item["access_token"];
+        }
+    }
+}
---
+++ TestPinterest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPinterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pinterest = new Pinterest
+            {
+                AppID = "5021144650893801612",
+                AppIDSecret = "ba3a73bd5aab2032c03535e2d0d29d059bc41a96d4efdf8e109ea32629fb2a95",
+                AccessToken= "Ane9FdYP7YJNPdqqG8Rr5iTgW5utFYqud3VlVcBFrrB08mC4jAm_wDAAAiGTRa6zDAbAr7UAAAAA"
+
+            };
+            IEnumerable<string> pins = pinterest.GetPins("https://api.pinterest.com/v1/users/GcSocialmediatest/", "Gc", "Socialmediatest").Result;
+            foreach (var p in pins)
+            {
+                Console.WriteLine(p + "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
